1bc1d3c5c2089b6670be62100c8cb8a6239efa52
Kyle Lierer <36929178+Kyle-L@users.noreply.github.com>
2021-07-29 19:25:33 -0400

    Added a element fade effect.

---
+++ src/assets/js/fiddle.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $('.fadeMe').each( function(i){
+        var bottom_of_object = $(this).position().top + ($(this).outerHeight())/3;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_object ){
+            $(this).css('opacity', '1');
+        }
+    });
+
+    $(window).scroll( function(){
+        $('.fadeMe').each( function(i){
+            var bottom_of_object = $(this).position().top + ($(this).outerHeight())/3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
+});
