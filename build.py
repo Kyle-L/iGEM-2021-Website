2250852bf57d18e0b0626f0d84022f63c5e70f84
Kyle Lierer <36929178+Kyle-L@users.noreply.github.com>
2021-08-12 14:08:55 -0400

    Added quality of life feautures.
    - Converting local links to absolute. ('iGEM server does not support local.')
    - Sets non-local links to open in a new browser.

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 import argparse
-import os
-from staticjinja import Site
-from pathlib import Path
+from bs4 import BeautifulSoup
 import htmlmin
+import os
+from pathlib import Path
+from staticjinja import Site
+import re
 
 
 def _page_context(template):
@@ -17,55 +19,102 @@ def _page_render(site, template, **kwargs):
     site.get_template(".base.html").stream(**kwargs).dump(str(out), encoding="utf-8")
 
 
-def _minimize(outpath):
-    path = outpath
-
-    files = []
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.html' in file or '.css' in file:
-                files.append(os.path.join(r, file))
-
-    for f in files:
-        print(f'Minimizing {f}')
-        minified = htmlmin.minify(open(f).read(), remove_empty_space=False, remove_comments=True)
-        textfile = open(f, 'w')
-        textfile.write(minified)
-        textfile.close()
+def _minimize(html):
+    return htmlmin.minify(html, remove_empty_space=False, remove_comments=True)
 
 
-def build(outpath, searchpath):
+def _replace_local_links_with_absolute(html, src_link="https://2021.igem.org/Team:MiamiU_OH"):
+    pattern = "(^\/.*$)"
+
+    soup = BeautifulSoup(html, features="lxml")
+
+    for a in soup.findAll('a'):
+        if re.match(pattern , a['href']) or not a['href']:
+            a['href'] = a['href'].replace(a['href'], src_link + a['href'])
+
+    return str(soup)
+    
+
+def _set_link_target(html, open_external_links_in_new_tab = True, open_internal_links_in_new_tab = False):
+    pattern = "(^\/.*$)"
+
+    soup = BeautifulSoup(html, features="lxml")
+
+    for a in soup.findAll('a'):
+        if re.match(pattern , a['href']) or not a['href']:
+            del a['target']
+        else:
+            a['target'] = "#blank"
+
+    return str(soup)
+
+def build(build_path, src_path):
+    """
+    Builds a site using the staticjinja templating engine.
+    Also performs quality of life operations for iGEM including:
+        - Converting local links to absolute. ('iGEM server does not support local.')
+        - Sets non-local links to open in a new browser.
+        - Minimize html and css files to speed up load time.
+
+    Args:
+        build_path (str): The output directory of the built wiki.
+        src (str): The source wiki that is being built.
+    """
+
+    # Builds the site with staticjinja.
     site = Site.make_site(
-        searchpath=searchpath,
-        outpath=outpath,
+        searchpath=src_path,
+        outpath=build_path,
         contexts=[(".*\.html", _page_context)],
         rules=[(".*\.html", _page_render)],
         staticpaths=["assets"]
     )
     site.render()
-
-    _minimize(outpath)
     
+    # With the site built, performs quality of life modifications.
+    files = []
+    for r, d, f in os.walk(build_path):
+        for file in f:
+            if '.html' in file or '.css' in file:
+                files.append(os.path.join(r, file))
+
+    for f in files:
+        html = open(f).read()
+
+        if '.html' in f:
+            print(f'Setting links targets for {f}')
+            html = _set_link_target(html)
+
+            print(f'Replacing local links with absolute for {f}')
+            html = _replace_local_links_with_absolute(html)
+
+        print(f'Minimizing {f}')
+        html = _minimize(html)
+
+        textfile = open(f, 'w')
+        textfile.write(html)
+        textfile.close()
+
 
 if __name__ == '__main__':
     """
     If run as a main file (i.e., `python ./build.py`), Builds the local wiki
 
     positional arguments:
-        build-directory  The output directory of the built wiki.
-        src              The source wiki that is being built.
+        build-path    The output path of the built wiki.
+        src-path      The source path wiki that is being built.
 
     """
 
     parser = argparse.ArgumentParser(description='Builds the local wiki.')
 
     parser.add_argument('Build',
-                        metavar='build-directory',
+                        metavar='build-path',
                         type=str,
-                        help='The output directory of the built wiki.')
+                        help='The output path of the built wiki.')
 
     parser.add_argument('Site',
-                        metavar='src',
+                        metavar='src-path',
                         type=str,
                         help='The source wiki that is being built.')
 
