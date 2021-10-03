77a74bbfa36f95b5ef50d6491607afe56925a8ad
Kyle Lierer <36929178+Kyle-L@users.noreply.github.com>
2021-10-02 20:03:00 -0400

    Updated fiddle animation.

--- src/assets/js/fiddle.js
+++ src/assets/js/fiddle.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 
-    // $('div').each( function(i){
-    //     $(this).css('opacity', '0');
-    // });
+    $('div').each( function(i){
+        $(this).css('opacity', '0');
+    });
 
     $('.fadeMeInstantly').each( function(i){
         $(this).animate({'opacity':'1'},500);
     });
 
-    // $('div').each( function(i){
-    //     var bottom_of_object = $(this).position().top + ($(this).outerHeight())/8;
-    //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //     if( bottom_of_window > bottom_of_object ){
-    //         $(this).css('opacity', '1');
-    //     }
-    // });
+    $('div').each( function(i){
+        var bottom_of_object = $(this).position().top + ($(this).outerHeight())/8;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > bottom_of_object ){
+            $(this).css('opacity', '1');
+        }
+    });
 
-    // $(window).scroll( function(){
-    //     $('*').each( function(i){
-    //         var bottom_of_object = $(this).position().top + ($(this).outerHeight())/8;
-    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //         if( bottom_of_window > bottom_of_object ){
-    //             $(this).animate({'opacity':'1'},500);
-    //         }
-    //     });
-    // });
+    $(window).scroll( function(){
+        $('div').each( function(i){
+            var bottom_of_object = $(this).position().top + ($(this).outerHeight())/8;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
 });
